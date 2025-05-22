6ce5b06c5b619cc296f5d5ae7386276833b36aaa
Anand Mishra <ctfu.anand@outlook.com>
2025-05-22 15:57:34 +0530

    update ALLOWED_HOSTS to allow all origins during development

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k+g5a18i)p%$+0#%t$0jn6s$dvq-iz8ev&57x%+*_808beaedw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
